4cf4f853f184b3566e9b2be9bdc08b34f087b76b
Praneeth <ambatisaipraneeth@gmail.com>
2017-02-07 11:08:03 -0600

    Implemented Logout through Options Menu

--- app/src/main/java/com/expensetracker/praneethambati/expensetracker/LandingActivity.java
+++ app/src/main/java/com/expensetracker/praneethambati/expensetracker/LandingActivity.java
@@ -1,14 +1,23 @@
 package com.expensetracker.praneethambati.expensetracker;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 public class LandingActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button insertBTN,showBTN;
+    public static final String MyPREFERENCES = "MyPrefs";
+    public static final String userKey = "";
+    public static final String passwordKey = "";
+    public static final String profileKey = "";
+
+    Button insertBTN,showBTN, createGroupBTN, showGroupBTN;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,10 +25,14 @@ public class LandingActivity extends AppCompatActivity implements View.OnClickLi
 
         insertBTN = (Button) findViewById(R.id.insertBTN);
         showBTN = (Button) findViewById(R.id.showBTN);
+        createGroupBTN = (Button) findViewById(R.id.createGroupBTN);
+        showGroupBTN = (Button) findViewById(R.id.showGroupBTN);
 
 
         insertBTN.setOnClickListener(this);
         showBTN.setOnClickListener(this);
+        createGroupBTN.setOnClickListener(this);
+        showGroupBTN.setOnClickListener(this);
     }
 
     @Override
@@ -34,6 +47,37 @@ public class LandingActivity extends AppCompatActivity implements View.OnClickLi
                 Intent show = new Intent(LandingActivity.this,ShowSavingsActivity.class);
                 startActivity(show);
                 break;
+            case R.id.createGroupBTN:
+                Intent create = new Intent(LandingActivity.this,CreateGroupActivity.class);
+                startActivity(create);
+                break;
+            case R.id.showGroupBTN:
+                Intent showg = new Intent(LandingActivity.this,ShowGroupActivity.class);
+                startActivity(showg);
+                break;
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.mainmenu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int myItem = item.getItemId();
+
+        switch (myItem){
+            case R.id.logout:
+                SharedPreferences.Editor sharedPreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE).edit();
+                sharedPreferences.clear();
+                sharedPreferences.apply();
+                Intent i = new Intent(LandingActivity.this,LoginActivity.class);
+                startActivity(i);
+        }
+
+        return false;
     }
 }
